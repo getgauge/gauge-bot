659b1ab725de0aa481b70b9fc0333dc514b501b3
Nehashri <neha.nehe@gmail.com>
2019-09-11 20:41:05 +0530

    creating pr review request automatically

--- src/prHandler.js
+++ src/prHandler.js
@@ -18,38 +18,64 @@ async function createStatus(context, state, recheck) {
   return context.github.repos.createStatus(status);
 }
 
-async function updatePR(context, unsignedUsers, recheck) {
-  if (unsignedUsers.length === 0 || (unsignedUsers.length === 1 && unsignedUsers[0].includes('[bot]'))) {
-    return createStatus(context, true, recheck);
-  }
-  let users = [];
-  unsignedUsers.forEach(u => {
-    let user = `@${u}`;
-    if (!users.includes(user))
-      users.push(user);
-  });
+async function updateClaStatusForUnsignedUsers(context, unsignedUsers) {
+  let users = unsignedUsers.map(u => `@${u}`);
   await comments.addComment(context, messages.claNotice(users.join(', ')));
   return createStatus(context, false);
 }
 
 async function prCreated(context, recheck) {
+  let users = await getCommitUsers(context);
+  let unsignedUsers = getUnsignedUsers(users);
+  if (unsignedUsers.length === 0) {
+    await createStatus(context, true, recheck);
+    return createPullRequestReview(context);
+  }
+  return updateClaStatusForUnsignedUsers(context, unsignedUsers, recheck)
+}
+
+async function createPullRequestReview(context, users) {
+  const ownerLogin = context.payload.organization.login;
+  const repoName = context.payload.repository.name;
+  let reviewTeam = (await context.github.repos.listTeams({owner: ownerLogin, repo: repoName}))
+    .data.find(team => team.name == "reviewers");
+  if (!reviewTeam) return;
+  let members = (await context.github.teams.listMembers({team_id: reviewTeam.id})).data;
+  let mem = members.filter(member => !users.includes(member.login));
+  let reviewer = mem[Math.floor(Math.random() * mem.length)].login;
+  await context.github.pullRequests.createReviewRequest({ 
+    owner: ownerLogin, 
+    repo: repoName, 
+    number: context.payload.pull_request.number, 
+    reviewers: [reviewer]
+  });
+}
+
+async function getUnsignedUsers(users) {
+  let unsignedUsers = [];
+  for (let user in users) {
+    if (!user.includes('[bot]') && !(await data.hasSignedCLA(user))){
+      unsignedUsers.push(user);
+    }
+  }
+}
+
+async function getCommitUsers(context) {
   const compare = await context.github.repos.compareCommits(context.repo({
     base: context.payload.pull_request.base.sha,
     head: context.payload.pull_request.head.sha
   }));
-  let unsignedUsers = [];
+  let users = [];
   for (const { author, committer } of compare.data.commits) {
     if (!author || !committer) continue;
     let authorLogin = author.login;
     let committerLogin = committer.login;
     if (authorLogin !== committerLogin && committerLogin !== 'web-flow') {
-      let signed = await data.hasSignedCLA(committerLogin);
-      !signed && unsignedUsers.push(committerLogin);
+      if (!users.includes(committerLogin)) users.push(committerLogin);
     }
-    let signed = await data.hasSignedCLA(authorLogin);
-    !signed && unsignedUsers.push(authorLogin);
+    if (!users.includes(authorLogin)) users.push(authorLogin);
   }
-  return updatePR(context, unsignedUsers, recheck);
+  return users;
 }
 
 module.exports = prCreated;
