c9a694dd1a88c1b43b4926a4a8f8e1158f461621
Zabil Cheriya Maliackal <zabilcm@gmail.com>
2022-12-17 21:43:42 +0000

    Add netlify function
    Signed-off-by: Zabil Cheriya Maliackal <zabilcm@gmail.com>

---
+++ functions/index.js
@@ -0,0 +1,7 @@
+import { createLambdaFunction, createProbot } from "@probot/adapter-aws-lambda-serverless";
+
+import appFn from "../index";
+
+export const handler = createLambdaFunction(appFn, {
+  probot: createProbot(),
+});
